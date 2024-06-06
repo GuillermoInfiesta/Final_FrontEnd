54910580054df637aad34efc13b92da2de5bc209
GuillermoInfiesta <ginfiestap@gmail.com>
2024-06-06 23:14:12 +0200

    Check

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
       <h1>Final FrontEnd</h1>
       <p>Buenos dias, noches o lo que quiera que sea</p>
+      <p>Prueba de que esto actualiza con el commit, no vaya a ser</p>
     </div>
   );
 }
